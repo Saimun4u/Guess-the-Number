17cc4cae442cb35f0b2af60197feb5deb22318c7
Saimun Hassan <saimun4u@gmail.com>
2023-03-11 21:01:35 -0500

    Develop an alternate function to guess number

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ import random
 # num = random.randint(1,10)
 # print(num)
 
-def guess_number():
-    num = random.randint(1,5)
-    guess = int(input('Guess the number the computer generated between 1 and 5: '))
-    while True:
-      if guess != num:
-          print('Your guess is wrong🤣 Better luck next time')
-          res = input("Do you want to try again - Yes-y, No-n ")
-          if res == "y":
-             guess = int(input('Guess the number: '))
-          elif res == "n":
-             break
-      else:
-        print("Congratulations😊! You've guessed the correct number")
-        res = input("Do you want to try again - Yes-y, No-n ")
-        if res == "y":
-           guess_number()
-        elif res == "n":
-           print('*****See you soon*****!')
-           break
+# *** FIRST SOLUTION ***
 
-guess_number()
+# def guess_number():
+#     num = random.randint(1,5)
+#     guess = int(input('Guess the number the computer generated between 1 and 5: '))
+#     while True:
+#       if guess != num:
+#           print('Your guess is wrong🤣 Better luck next time')
+#           res = input("Do you want to try again - Yes-y, No-n ")
+#           if res == "y":
+#              guess = int(input('Guess the number: '))
+#           elif res == "n":
+#              break
+#       else:
+#         print("Congratulations😊! You've guessed the correct number")
+#         res = input("Do you want to try again - Yes-y, No-n ")
+#         if res == "y":
+#            guess_number()
+#         elif res == "n":
+#            print('*****See you soon*****!')
+#            break
+
+# guess_number()
+
+# *** SECOND SOLUTION ***
